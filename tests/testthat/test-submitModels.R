12ab5b5ac2b7d9a9bcc4a2ec5e9661a1c654a4df
Michael Hallquist <michael.hallquist@gmail.com>
2025-08-23 15:06:26 -0400

    One more fix to tests for CRAN

--- tests/testthat/test-submitModels.R
+++ tests/testthat/test-submitModels.R
@@ -1,24 +1,28 @@
 # tests for submitModels
 p <- test_path("submitModels")
 
-# checks on parsing of scheduling arguments and script setup
-track <- submitModels(p, sched_args=c("--mail=user", "--export=v"), debug=TRUE, max_time_per_job = "4:10:00", combine_jobs = TRUE)
+test_that("submitModels job ID check", {
+  # checks on parsing of scheduling arguments and script setup
+  mplus_fake <- tempfile()
+  file.create(mplus_fake)
+  track <- submitModels(p, sched_args=c("--mail=user", "--export=v"), debug=TRUE, 
+                        max_time_per_job = "4:10:00", combine_jobs = TRUE, Mplus_command = mplus_fake)
+  
+  expect_equal(track$jobid[1], "dummy_1")
+  
+  # job allocation checks
+  expect_equal(track$memgb[1], 4)
+  expect_equal(track$cores[1], 1)
+  expect_equal(track$wall_time[1], "3:00:00")
+  
+})
 
 # single model
-inp <- file.path(p, "ex3.1.inp")
+# inp <- file.path(p, "ex3.1.inp")
+# 
+# submitModels(inp, sched_args=c("--mail=user", "--export=v"), debug=TRUE, 
+#              max_time_per_job = "2:10:00", combine_jobs = TRUE, replace="always", Mplus_command = mplus_fake)
 
-submitModels(inp, sched_args=c("--mail=user", "--export=v"), debug=TRUE, max_time_per_job = "2:10:00", combine_jobs = TRUE, replace="always")
-
-test_that("submitModels job ID check", {
-    expect_equal(track$jobid[1], "dummy_1")
-})
-
-test_that("submitModels job allocation checks", {
-    expect_equal(track$memgb[1], 4)
-    expect_equal(track$cores[1], 1)
-    expect_equal(track$wall_time[1], "3:00:00")
-    
-})
 
 # setwd("/proj/mnhallqlab/users/michael/MplusAutomation")
 # checks on cluster (must be run on slurm cluster!)
@@ -36,8 +40,12 @@ test_that("submitModels job allocation checks", {
 
 # combine jobs challenge
 p <- test_path("submitModels/job_combine")
-track <- submitModels(p, sched_args=c("--mail=user", "--export=v"), debug=TRUE, max_time_per_job = "48:10:00", batch_outdir = file.path(p, "batchfiles"))
+
 test_that("submitModels combines jobs as expected", {
+  mplus_fake <- tempfile()
+  file.create(mplus_fake)
+  track <- submitModels(p, sched_args=c("--mail=user", "--export=v"), debug=TRUE, max_time_per_job = "48:10:00",
+                        batch_outdir = file.path(p, "batchfiles"), Mplus_command = mplus_fake)
   expect_equal(track$file[[5]], c("job_19.inp", "job_20.inp"))
   expect_equal(track$wall_hr[5], 39)
 })
