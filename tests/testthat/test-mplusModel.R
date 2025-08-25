3acd1a2e0f9915eece451d64cfc7a812c0075ba4
Michael Hallquist <michael.hallquist@gmail.com>
2025-08-25 17:44:14 -0400

    Shift to markdown; new expandCmd that is more robust; mplusModel now has all fields from readModels

--- tests/testthat/test-mplusModel.R
+++ tests/testthat/test-mplusModel.R
@@ -62,6 +62,6 @@ test_that("mplusModel exposes readModels sections", {
   file.create(mplus_fake)
   m <- mplusModel(inp_file = file.path(tmp, "ex3.1.inp"), read = TRUE, Mplus_command = mplus_fake)
   expect_true(length(m$output) > 0)
-  expect_true(is.null(m$errors))
+  expect_true(length(m$errors) == 0L)
 })
 
